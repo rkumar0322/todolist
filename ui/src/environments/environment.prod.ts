6a3229f98e538e107c719b228029285a37db5c2c
Rahul Kumar <rk32298@gmail.com>
2020-06-28 23:04:38 -0400

    frontend

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  gateway: 'http://localhost:3000',
+  callback: 'http://localhost:4200/callback',
+  domain: 'rkumar0322.us.auth0.com',
+  clientId: 'HAElBvZ6UwKCPS72fOikPlyPMqKDUmDp',
+  audience: 'https://todolist'
+};
